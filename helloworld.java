7424545e2bec60d856c3ed92e3c7623f87c44d49
saifullahumfagur <77085169+saifullahumfagur@users.noreply.github.com>
2021-01-19 10:30:05 +0800

    Update helloworld.java
    output"editan baru"

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,9 @@
 public class Helloworld{
    pbulic static void main(){
    System.out.println("hello world");
+      System.out.println("editan baru");
+      System.out.println("editan baru");
+      
    }
 }   
 bang iful jamil
